e8b1fcfbbe03f8c116296e8078b44881ac59e8a1
Ankita Khurana <ankita0096@gmail.com>
2018-09-25 17:37:37 +0530

    Events Tab

---
+++ src/components/EventCard.vue
@@ -0,0 +1,22 @@
+<template>
+     <div class="card">       
+         <h1> {{event.name}} </h1>        
+         <img :src=event.img_url class="image">        
+     </div>
+</template>
+
+
+<style>
+.image {
+  height: 200px;
+  width: 200px;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    event: Object
+  }
+};
+</script>
---
+++ src/views/Events.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+<h1>Events</h1> 
+<div class="outer">
+    
+        <dir v-for="event in events" :key="event.id" > 
+           <EventCard  :event="event"/>
+        </dir>
+        </div>
+        </div>
+</template>
+
+<style>
+.outer{
+  display:flex
+}
+</style>
+
+<script>
+import json from '@/data.json'
+import EventCard from "@/components/EventCard";
+export default {
+  name: "events",
+   data: function () {
+    return {
+      title:"Application",
+      events:json
+    }
+  },
+  components: {
+    EventCard
+  }
+};
+</script>
